a889f013647dbe54c988cd9199aee6aac6f5a92e
GustavoMatsunaga <63619508+GustavoMatsunaga@users.noreply.github.com>
2020-09-23 11:00:36 -0300

    FEATURES: Double Click, New Task, Responsive Web Design; README: Added Readme

---
+++ README.md
@@ -0,0 +1,45 @@
+# Welcome! 👋
+
+![Language count badge](https://img.shields.io/github/languages/count/GustavoMatsunaga/ToDoList)
+![Code size badge](https://img.shields.io/github/languages/code-size/GustavoMatsunaga/ToDoList)
+![Code last commit](https://img.shields.io/github/last-commit/GustavoMatsunaga/ToDoList)
+
+Since I'm an organized person, I like to always know what I need to do in the day so I created this To-Do-List to help me with my daily routines.
+</br>
+You can checkout the demo here : <strong><a href="https://gustavomatsunaga.github.io/ToDoList/" target="_blank">To Do List</a></strong>
+
+## Challenges! :pushpin:
+
+The application was developed with only HTML, CSS and vanilla Javascript. I made a CRUD (Create, read, update and delete) application in order to maintain my daily routines organized. The website is completely responsive, so it can fit even for mobiles. 
+
+**Features! Let's have fun!** 🚀
+
+<ul>
+    <li>View the optimal layout for the component depending on their device's screen size (Responsive Web Design)</li>
+    <li>New Task Button => Add a new task</li>
+    <li>Double Click => Edit or delete the Task</li>
+</ul>
+
+## Technologies! :computer:
+
+<ul>
+    <li>HTML</li>
+    <li>SASS</li>
+    <li>JAVASCRIPT</li>
+</ul>
+
+# Web Model
+## Model
+
+![Gif preview for the desktop Color Mark](./images/github/desktopModel.png)
+
+## Desktop
+
+![Gif preview for the desktop Color Mark](./images/github/desktop.gif)
+
+## Mobile
+
+<div >
+<img src="images/github/mobileModel.png" width="250" height="350" align="left">
+<img src="images/github/mobile.gif" width="250" height="350" align="right">
+</div>
--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,139 @@
+/********* MAIN PAGES *********/
 var taskes = document.querySelector(".taskes");
-var taskEdit = document.querySelector("#edit");
-var taskCreate = document.querySelector("#create");
-var main = document.querySelector(".main")
+var editPage = document.querySelector("#edit");
+var createPage = document.querySelector("#create");
+var main = document.querySelector(".main");
 
-var saveCreate = document.querySelector(".create-button-save")
-var deleteCreate = document.querySelector(".create-button-save")
+/********* CREATE AREA *********/
+var taskCreate = document.querySelector("#task-card-text-create");
+var saveCreate = document.querySelector(".create-button-save");
+var deleteCreate = document.querySelector(".create-button-delete");
 
-// ADD post-it
+
+/********* EDIT AREA *********/
+var taskEdit = document.querySelector("#task-card-text-edit");
+var saveEdit = document.querySelector(".edit-button-save");
+var deleteEdit = document.querySelector(".edit-button-delete");
+
+/********* GLOBAL VARIABLES *********/
+var taskesArray = [];
+var idGlobal = 0;
+
+/********* MAIN PAGE *********/
+// ADD TASK
 var addButton = document.querySelector(".add")
 addButton.addEventListener("click", () => {
-    taskCreate.classList.toggle("notshow");
+    createPage.classList.toggle("notshow");
     main.classList.toggle("notshow");
 })
 
-// CREATE post-it
+// SAVE TASK
 saveCreate.addEventListener("click", () => {
-    taskCreate.classList.toggle("notshow");
-    main.classList.toggle("notshow");
+    togglePageCreate();
+    taskesArray.push(taskCreate.value)
+    updatePostIt();
+    taskCreate.value = ""
+})
+
+// DELETE TASK (MAIN PAGE)
+deleteCreate.addEventListener("click", () => {
+    taskCreate.value = ""
+    togglePageCreate();
 })
 
 
 
-
-
-
-
-
-
-
-function createPostIt() {
+/********* FUNCTIONS *********/
+// CREATE THE POST IT BY USING THE INFORMATION GIVE BY THE USER GETTING IT`S VALUE
+function createPostIt(task) {
+    // Create the task card
     var taskCard = document.createElement("div");
     taskCard.classList.add("task-card");
+    
+    // Create the task text
     var taskCardText = document.createElement("p");
     taskCardText.classList.add(".task-card-text")
+    taskCardText.textContent = task;
+
+    // Generates random positions to make the Post-It more dynamic
+    taskCard.style.transform = `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+
+    // Insert the text on the card
+    taskCard.appendChild(taskCardText);
+    
+    return taskCard;
 }
+
+// UPDATE THE VALUES
+function updatePostIt () {
+    // First clean all the content that there is on taskes
+    taskes.textContent = "";
+
+    // Make sure that the ID is 0, so whenever it update the values the ID stay in order
+    var i = 0;
+
+    // The actual task will receive the values from the task in taskes and put the ID
+    var actualTask = ""
+    taskesArray.forEach(task => {
+        actualTask = createPostIt(task)
+        actualTask.id = i;
+        taskes.appendChild(actualTask);
+        i++;
+    });
+
+    // Adding the event listener of double click
+    addEditListener();
+}
+
+/********* Toggle the page create to the main page *********/
+function togglePageCreate () {
+    createPage.classList.toggle("notshow");
+    main.classList.toggle("notshow");
+}
+
+/********* Toggle the page create to the edit page *********/
+function togglePageEdit () {
+    editPage.classList.toggle("notshow");
+    main.classList.toggle("notshow");
+}
+
+/********* Adding the event listener double click *********/
+function addEditListener () {
+    // Adding edit and delete function with a event listener of double click
+    var taskCards = document.querySelectorAll(".task-card");
+    taskCards.forEach(taskCard => {
+    taskCard.addEventListener("dblclick", editTask);
+    })
+}
+
+/********* Edit Task *********/
+function editTask (event) {
+    togglePageEdit ()
+
+    // Gets the element that is going to be updated
+    var postIt = event.target
+
+    // Change the content of the box of the task Edit to the Post-It that is going to be edit
+    taskEdit.value = postIt.textContent;
+
+    // Put the ID in a Global ID, so other functions have the access to the element that is being updated
+    idGlobal = postIt.id;
+}
+
+deleteEdit.addEventListener("click", () => {
+    // Delete the element using the global ID
+    taskesArray.splice(idGlobal, 1);
+
+    // Update the elements
+    updatePostIt();
+    togglePageEdit();
+})
+
+saveEdit.addEventListener("click", () => {
+    // Change the content of the element by using the Global ID
+    taskesArray[idGlobal] = taskEdit.value;
+    // Update the elements
+    updatePostIt();
+    // Insert the default value to the task edit content
+    togglePageEdit();
+})
